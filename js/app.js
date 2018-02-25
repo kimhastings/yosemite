ce67df86b735e052bb2c3b248c5cc1b12cc74880
Kim Hastings <kim@stikine.org>
2018-02-25 00:43:44 -0800

    Passes jshint

--- js/app.js
+++ js/app.js
@@ -3,89 +3,89 @@
 var Model = {
     markers: [
       {
-        title: 'Ahwahnee Hills Park Trails',
-        url:'https://www.yosemitehikes.com/not-yosemite/ahwahnee-park/ahwahnee-park.htm',
-        lat: 37.380380, 
+        title: "Ahwahnee Hills Park Trails",
+        url:"https://www.yosemitehikes.com/not-yosemite/ahwahnee-park/ahwahnee-park.htm",
+        lat: 37.380380,
         lng: -119.731692
       },
       {
-        title: 'Hite Cove Trail',
-        url:'https://www.yosemitehikes.com/not-yosemite/hite-cove/hite-cove.htm',
-        lat: 37.654436, 
+        title: "Hite Cove Trail",
+        url:"https://www.yosemitehikes.com/not-yosemite/hite-cove/hite-cove.htm",
+        lat: 37.654436,
         lng: -119.887481
       },
       {
-        title: 'Merced River Trail (Briceburg)',
-        url:'https://www.blm.gov/visit/merced-river',
-        lat: 37.604710,  
+        title: "Merced River Trail (Briceburg)",
+        url:"https://www.blm.gov/visit/merced-river",
+        lat: 37.604710,
         lng: -119.968021
       },
       {
-        title: 'Merced River Trail (Railroad Flat)',
-        url: 'https://www.blm.gov/visit/merced-river',
-        lat: 37.618932,  
+        title: "Merced River Trail (Railroad Flat)",
+        url: "https://www.blm.gov/visit/merced-river",
+        lat: 37.618932,
         lng: -120.019793
       },
       {
-        title: 'Stockton Creek Trail System',
-        url: 'https://sierrafoothill.org/stockton-creek-preserve/',
-        lat: 37.503194,  
+        title: "Stockton Creek Trail System",
+        url: "https://sierrafoothill.org/stockton-creek-preserve/",
+        lat: 37.503194,
         lng: -119.967228
       },
       {
-        title: 'Miami Mountain Trail',
-        url: 'http://www.fire-lookouts.org/cali/miami_mtn/index.htm',
-        lat: 37.417556,  
+        title: "Miami Mountain Trail",
+        url: "http://www.fire-lookouts.org/cali/miami_mtn/index.htm",
+        lat: 37.417556,
         lng: -119.730950
       },
       {
-        title: 'Goat Mountain Trail',
-        url: 'http://www.nhlr.org/lookouts/us/ca/goat-mountain-lookout/',
-        lat: 37.291745,  
+        title: "Goat Mountain Trail",
+        url: "http://www.nhlr.org/lookouts/us/ca/goat-mountain-lookout/",
+        lat: 37.291745,
         lng: -119.579649
       },
       {
-        title: 'Lewis Creek Trail',
-        url: 'https://www.yosemitehikes.com/not-yosemite/lewis-creek/lewis-creek.htm',
-        lat: 37.403548,   
+        title: "Lewis Creek Trail",
+        url: "https://www.yosemitehikes.com/not-yosemite/lewis-creek/lewis-creek.htm",
+        lat: 37.403548,
         lng: -119.625869
       },
       {
-        title: 'Nelder Grove Trail System',
-        url: 'https://www.yosemitehikes.com/not-yosemite/nelder-grove/nelder-grove.htm',
-        lat: 37.430436,   
+        title: "Nelder Grove Trail System",
+        url: "https://www.yosemitehikes.com/not-yosemite/nelder-grove/nelder-grove.htm",
+        lat: 37.430436,
         lng: -119.584711
       },
       {
-        title: 'Preston Falls Trail',
-        url: 'https://www.fs.usda.gov/recarea/stanislaus/recarea/?recid=14967',
-        lat: 37.878708,   
+        title: "Preston Falls Trail",
+        url: "https://www.fs.usda.gov/recarea/stanislaus/recarea/?recid=14967",
+        lat: 37.878708,
         lng: -119.950472
       },
       {
-        title: 'Red Hills Trail System',
-        url: 'https://www.blm.gov/visit/red-hills',
-        lat: 37.838470,   
+        title: "Red Hills Trail System",
+        url: "https://www.blm.gov/visit/red-hills",
+        lat: 37.838470,
         lng: -119.730950
       },
       {
-        title: 'Table Mountain Trail (Jamestown)',
-        url:'https://www.tchistory.org/tchistory/Wonders_9.htm',
-        lat: 37.943824,    
+        title: "Table Mountain Trail (Jamestown)",
+        url:"https://www.tchistory.org/tchistory/Wonders_9.htm",
+        lat: 37.943824,
         lng: -120.462019
       },
       {
-        title: 'Table Mountain Trail (Prather)',
-        url: 'https://sierrafoothill.org/ruth-mckenzie-table-mountain-preserve/',
-        lat: 37.049427,    
+        title: "Table Mountain Trail (Prather)",
+        url: "https://sierrafoothill.org/ruth-mckenzie-table-mountain-preserve/",
+        lat: 37.049427,
         lng: -119.565250
       },
       {
-        title: 'San Joaquin River Gorge Trail System',
-        url: 'https://www.blm.gov/visit/san-joaquin-river-gorge-recreation-area',
-        lat: 37.082906,     
+        title: "San Joaquin River Gorge Trail System",
+        url: "https://www.blm.gov/visit/san-joaquin-river-gorge-recreation-area",
+        lat: 37.082906,
         lng: -119.554113
-      },
+      }
     ]
 };
 
@@ -202,18 +202,18 @@ function populateInfoWindow(marker, infowindow) {
           var nearestCampground = {
             name: doc[0].children[0].attributes[6].nodeValue,
             url: "https://www.reserveamerica.com/campsiteSearch.do?contractCode=" + contractCode + "&parkId=" + facilityID
-          }
+          };
           console.log(nearestCampground);
 
           // Format the infowindow
-          var link = '</br></br><a href="' + marker.url +  '" target="_blank">Click here for details about this hike</a>' 
+          var link = '</br></br><a href="' + marker.url +  '" target="_blank">Click here for details about this hike</a>';
           infowindow.setContent('<div>' + marker.title + link + '</br></br>Nearest Campground: <a href=' + nearestCampground.url + ' target="_blank">' + nearestCampground.name + '</a></div>');
           // Open the infowindow on the correct marker.
           infowindow.open(map, marker);
         },
         error: function() {
           console.log('Unable to retrieve campground data');
-          var link = '</br></br><a href="' + marker.url +  '" target="_blank">Click here for details about this hike</a>' 
+          var link = '</br></br><a href="' + marker.url +  '" target="_blank">Click here for details about this hike</a>'; 
           infowindow.setContent('<div>' + marker.title + link + '</br></br>UNABLE TO FIND CAMPGROUND INFO</div>');
           infowindow.open(map, marker);
         }
@@ -223,8 +223,8 @@ function populateInfoWindow(marker, infowindow) {
       map.panTo(marker.getPosition());
       marker.setAnimation(google.maps.Animation.BOUNCE);
       setTimeout(function(){ marker.setAnimation(null); }, 2100);   
-    };
-};
+    }
+}
 
 function initialize() {
   /* Enable popovers (for About) */
